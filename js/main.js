42c8484a1e20dd82f817074c919098fe9e2b1f35
Alexander <alex.kadoh@mail.ru>
2021-06-06 12:16:08 +1000

    Рефакторинг заданмасиÐÑсиваÐ

--- js/main.js
+++ js/main.js
@@ -1,40 +1,5 @@
-
-const getRandomPositiveFloat = (min, max, digits = 1) => {
-  const lower = Math.min(Math.abs(min), Math.abs(max));
-  const upper = Math.max(Math.abs(min), Math.abs(max));
-  const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-};
-
-const getRandomPositiveInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const createElement = (array, index) => {
-  const currentElement = array[index];
-  array.splice(index,1);
-  return currentElement;
-};
-
-const createRandomArrayFeatures = (index) => {
-  const arrayFeatures = ALL_FEATURES.slice(index);
-  return arrayFeatures;
-};
-
-const allSymbols = '0123456789qwertyuiopasdfghjklzxcvbnm';
-
-const createRandomString = (length) => {
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += allSymbols[getRandomPositiveInteger(0, allSymbols.length - 1)];
-  }
-  return result;
-};
-
-
+const ALL_SYMBOLS = '0123456789qwertyuiopasdfghjklzxcvbnm';
+const MAX_PHOTOS = 100;
 
 const NUMBERS_FOR_URL = ['01', '02', '03', '04', '05', '06', '07', '08'];
 const TITLES = [
@@ -75,20 +40,48 @@ const DESCRIPTIONS = [
   'В собственной ванной комнате предоставляются бесплатные туалетно-косметические принадлежности.',
 ];
 
-const PHOTOS = [
-  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-${createRandomString(getRandomPositiveInteger(0, allSymbols.length))}.jpg`,
-  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-${createRandomString(getRandomPositiveInteger(0, allSymbols.length))}.jpg`,
-  `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-${createRandomString(getRandomPositiveInteger(0, allSymbols.length))}.jpg`,
-];
-const maxPhotos = 100;
+const getRandomPositiveFloat = (min, max, digits = 1) => {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(digits);
+};
+
+const getRandomPositiveInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createElement = (array, index) => {
+  const currentElement = array[index];
+  array.splice(index,1);
+  return currentElement;
+};
+
+const createRandomString = (element, length) => {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += element[getRandomPositiveInteger(0, element.length - 1)];
+  }
+  return result;
+};
+
 const createRandomPhotosArray = (index) => {
-  let photosArray = [];
+  const photos = [
+    `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-${createRandomString(ALL_SYMBOLS, getRandomPositiveInteger(0, ALL_SYMBOLS.length))}.jpg`,
+    `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-${createRandomString(ALL_SYMBOLS, getRandomPositiveInteger(0, ALL_SYMBOLS.length))}.jpg`,
+    `https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-${createRandomString(ALL_SYMBOLS, getRandomPositiveInteger(0, ALL_SYMBOLS.length))}.jpg`,
+  ];
+  const photosArray = [];
   for (let i = 0; i <= index - 1; i++){
-    photosArray.push(PHOTOS[getRandomPositiveInteger(0, PHOTOS.length - 1)]);
+    photosArray.push(photos[getRandomPositiveInteger(0, photos.length - 1)]);
   }
   return photosArray;
 };
 
+const createRandomArrayFeatures = (array, index) => array.slice(index);
 
 const createAdvertisement = () => (
   { author : {
@@ -103,9 +96,9 @@ const createAdvertisement = () => (
     guests: getRandomPositiveInteger(1,10),
     checkIn: CHECKS[getRandomPositiveInteger(0, CHECKS.length - 1)],
     checkOut: CHECKS[getRandomPositiveInteger(0, CHECKS.length - 1)],
-    features: createRandomArrayFeatures(getRandomPositiveInteger(1, ALL_FEATURES.length - 1)),
+    features: createRandomArrayFeatures(ALL_FEATURES, getRandomPositiveInteger(1, ALL_FEATURES.length - 1)),
     description: DESCRIPTIONS[getRandomPositiveInteger(0, DESCRIPTIONS.length - 1)],
-    photos: createRandomPhotosArray(getRandomPositiveInteger(1, maxPhotos)),
+    photos: createRandomPhotosArray(getRandomPositiveInteger(1, MAX_PHOTOS)),
   },
   location: {
     lat: getRandomPositiveFloat(35.65, 35.70, 5),
@@ -115,5 +108,4 @@ const createAdvertisement = () => (
 
 const similarAdvertisement = new Array(8).fill(null).map(() => createAdvertisement());
 
-console.log(similarAdvertisement[0].offer.photos);
-
+console.log(similarAdvertisement);
